8115b4f3c311e3adcb55e70e58fc174795005ab1
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-29 17:35:45 +0700

    init for STM32 lab 2

---
+++ STM32LAB2/Core/Inc/software_timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 29, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+#include"main.h"
+
+extern int timer_flag[10];
+int timer_counter[10];
+void setTimer(int index, int counter);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
--- STM32LAB2/Core/Src/main.c
+++ STM32LAB2/Core/Src/main.c
@@ -19,7 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "software_timer.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -89,15 +89,16 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
+//TODO setTimer
   /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  //TODO excercise
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -222,7 +223,9 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	timerRun();
+}
 /* USER CODE END 4 */
 
 /**
