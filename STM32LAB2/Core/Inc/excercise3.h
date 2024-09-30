1177075e74db0885dab31eb0626cada2344c0da0
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-30 07:52:25 +0700

    update1

---
+++ STM32LAB2/Core/Inc/excercise3.h
@@ -0,0 +1,16 @@
+/*
+ * excercise3.h
+ *
+ *  Created on: 30 thg 9, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EXCERCISE3_H_
+#define INC_EXCERCISE3_H_
+
+#include "main.h"
+
+void display7SEG(int num);
+void update7SEG(int *index);
+
+#endif /* INC_EXCERCISE3_H_ */
