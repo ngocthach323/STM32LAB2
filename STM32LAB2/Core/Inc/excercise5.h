22fe313cd7c99471a3588b49095e9347bf60e0c6
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-09-30 08:12:27 +0700

    init for ex5

---
+++ STM32LAB2/Core/Inc/excercise5.h
@@ -0,0 +1,16 @@
+/*
+ * excercise5.h
+ *
+ *  Created on: Sep 30, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EXCERCISE5_H_
+#define INC_EXCERCISE5_H_
+
+#include "main.h"
+
+void display7SEG(int num);
+void updateClockBuffer();
+
+#endif /* INC_EXCERCISE5_H_ */
--- STM32LAB2/Core/Src/main.c
+++ STM32LAB2/Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "software_timer.h"
+#include "excercise5.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -93,12 +94,28 @@ int main(void)
   /* USER CODE END 2 */
 
   /* Infinite loop */
-//TODO setTimer
   /* USER CODE BEGIN WHILE */
+  int index_led = 0;
+  int hour = 15;
+  int minute = 8;
+  int second = 50;
   while (1)
   {
     /* USER CODE END WHILE */
-	  //TODO excercise
+	  second++;
+	  if (second >= 60) {
+		  second = 0;
+		  minute++;
+	  }
+	  if (minute >= 60) {
+		  minute = 0;
+		  hour++;
+	  }
+	  if (hour >= 24) {
+		  hour = 0;
+	  }
+	  updateCLockBuffer();
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -198,14 +215,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
                           |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA5 PA6 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  /*Configure GPIO pins : PA4 PA5 PA6 PA7
+                           PA8 PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
