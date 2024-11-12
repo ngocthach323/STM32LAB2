7e39730863dcc08108f6eed4ad158edc878c657a
thachpham2004cqt <thach.pham2004cqt@hcmut.edu.vn>
2024-11-12 20:50:48 +0700

    complete ex1

--- STM32LAB2/Core/Src/main.c
+++ STM32LAB2/Core/Src/main.c
@@ -95,14 +95,14 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-//  setTimer(0, 1000);
+//  setTimer(0, 500);
   while (1)
   {
     /* USER CODE END WHILE */
 //	  if (timer_flag[0] == 1) {
 //		  run_excercise1(status);
 //		  status = 1 - status;
-//		  setTimer(0, 1000);
+//		  setTimer(0, 500);
 //	  }
 
     /* USER CODE BEGIN 3 */
